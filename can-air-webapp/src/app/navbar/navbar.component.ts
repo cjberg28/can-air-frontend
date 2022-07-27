cc6e5e5601a55badc0cae436612ddf453bcf7324
cjberg28 <cjberg28@yahoo.com>
2022-07-27 18:27:15 -0500

    There's some bug with the modal. Just extra config stuff.

--- can-air-webapp/src/app/navbar/navbar.component.ts
+++ can-air-webapp/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { UrlSerializer } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { User } from '../models/User';
@@ -10,67 +10,79 @@ import { User } from '../models/User';
 })
 export class NavbarComponent implements OnInit {
 
-  items!: MenuItem[];
-  currentUser? :User;
-  isUserLoggedIn :boolean = true;
-  displayModal :boolean = false;
+  items: MenuItem[] = [];
+  currentUser?: User;
+  isUserLoggedIn: boolean = false;
+  displayModal: boolean = false;
 
 
   constructor() {
 
-   }
+  }
 
   ngOnInit(): void {
     this.items = [{
       label: 'Home'
     },
     { label: 'My Flights' },
-    { label: 'Login',
-      command: () => {this.giveLoginDialogBox();} },
-    { label: this.getUsername(),
+    {
+      label: 'Login',
+      command: () => { this.giveLoginDialogBox(); }
+    },
+    {
+      label: this.getUsername(),
       items: [{
         label: 'Sign Out',
-        command: () => {this.logout();}
-      }]}
+        command: () => { this.logout(); }
+      }]
+    }
 
 
-  ];
+    ];
 
-}
 
-giveLoginDialogBox() :void {
-  // alert('Logging in');
-  this.showModalDialog();
-  this.clickedLogin();
-  this.currentUser = new User("Atul Mishra");
-  this.ngOnInit();
-}
 
-getUsername() :string {
-  if (this.currentUser == undefined) {//If the user hasn't logged in...
-    return "Not Signed In";
-  } else {
-    return "Hello, " + this.currentUser.name;
   }
-}
 
-logout() :void {
-  alert('Logging out');
-  this.currentUser = undefined;
-  this.ngOnInit();
-  //Router link to Home component.
-}
 
-clickedLogin() :boolean {
-  this.isUserLoggedIn = !this.isUserLoggedIn;
-  return this.isUserLoggedIn;
-}
 
-showModalDialog() :void {
-  if (this.currentUser == undefined) {//If the user hasn't logged in...
-    this.displayModal = true;
+
+
+  giveLoginDialogBox(): void {
+    // alert('Logging in');
+    this.showModalDialog();
+    this.clickedLogin();
+    this.currentUser = new User("Atul Mishra");
+    this.ngOnInit();
   }
-}
+
+  getUsername(): string {
+    if (this.currentUser == undefined) {//If the user hasn't logged in...
+      return "Not Signed In";
+    } else {
+      return "Hello, " + this.currentUser.name;
+    }
+  }
+
+  logout(): void {
+    alert('Logging out');
+    this.currentUser = undefined;
+    this.displayModal = false;
+    this.ngOnInit();
+    //Router link to Home component.
+  }
+
+  clickedLogin(): boolean {
+    this.isUserLoggedIn = true;//this.isUserLoggedIn = !this.isUserLoggedIn;
+    console.log(this.isUserLoggedIn);
+    return this.isUserLoggedIn;
+  }
+
+  showModalDialog(): void {
+    if (this.currentUser == undefined) {//If the user hasn't logged in...
+      this.displayModal = true;
+    }
+  }
 
 
 
