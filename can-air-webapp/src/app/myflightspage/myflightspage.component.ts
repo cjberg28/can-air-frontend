95da33ee5a9f1c263b391a65c6b63a5218fe04f2
Atul Mishra <69535893+friedfishra@users.noreply.github.com>
2022-08-04 16:42:43 -0400

    debugging reserveFlights and myFlights

--- can-air-webapp/src/app/myflightspage/myflightspage.component.ts
+++ can-air-webapp/src/app/myflightspage/myflightspage.component.ts
@@ -38,7 +38,7 @@ export class MyflightspageComponent implements OnInit {
 
   authorizedPerson: Person;
 
-  isUpdateSuccessful: any = false;
+  isUpdateSuccessful: any;
   isDeleteSuccessful: any = false;
 
   displayModal2: boolean = true;
